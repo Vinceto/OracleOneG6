f628136daf09f1915ee45986bb6fde80198e7d41
Ramon Bernabe Munoz Navarrete <ramon@recemed.com>
2024-01-24 16:22:39 -0300

    mejoras de codigo

--- 2034-logica-programacion-1-Aula1/app.js
+++ 2034-logica-programacion-1-Aula1/app.js
@@ -3,6 +3,11 @@ let numeroUsuario = prompt("Me indicas un número por favor:");
 
 console.log(numeroUsuario);
 
+/* ejemplo de comentario en un archivo javascript, los comentarios no se ejecutan se usan como recordatorio o explicacion */
+// otro ejemplo de comentario
+
 if (numeroUsuario == numeroSecreto) {
-    alert('Acertaste el número');
+    alert(`Acertaste el número ${numeroUsuario}`);
+}else{
+    alert(`Error, ${numeroUsuario} no es el numero. Intenta otra vez!`);
 }
